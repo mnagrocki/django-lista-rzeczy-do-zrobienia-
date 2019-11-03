71c94a8268d274809ff5297b41847b1a63c59f9e
Mario <mnagrocki@gmail.com>
2019-11-03 17:33:54 +0100

    dodamie modułu unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome(r'C:\Users\mnagr\PycharmProjects\TDD-poprawki\drivers\chromedriver.exe')
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTests (unittest.TestCase):
+    def setup(self):
+        self.browser =webdriver.Chrome(r'C:\Users\mnagr\PycharmProjects\TDD-poprawki\drivers\chromedriver.exe')
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Listy',self.browser.title)
+        self.fail('Zakończenie testu!')
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
+
